8abaa1eb4819c37a95506e399ccbee20343d2b9a
Alessandro Sangalli <alessandrosangalli@vipcommerce.com.br>
2023-10-28 22:20:41 -0300

    create table

---
+++ BodyCraftApi/Scripts/01 - CreateTables.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE `BodyCraftApiDb`.Foods (
+	Id INT UNSIGNED auto_increment NOT NULL,
+	Name varchar(100) NULL,
+	QuantityInGrams int NULL,
+	Protein float NULL,
+	Carb float NULL,
+	Fat float NULL,
+	CONSTRAINT Foods_PK PRIMARY KEY (Id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci;
+
+CREATE TABLE BodyCraftApiDb.Meals (
+	Id INT UNSIGNED auto_increment NOT NULL,
+	FoodId INT UNSIGNED NOT NULL,
+	`Time` TIME NOT NULL,
+	QuantityInGrams FLOAT NOT NULL,
+	CONSTRAINT Meals_PK PRIMARY KEY (Id),
+	CONSTRAINT Meals_Foods_FK FOREIGN KEY (FoodId) REFERENCES BodyCraftApiDb.Foods(Id) ON DELETE RESTRICT ON UPDATE RESTRICT
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci;
